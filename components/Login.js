ea9aabbaf8b06f7794c99f920a4fedca4098f045
MustArthur <artkrt@gmail.com>
2025-11-29 20:34:16 +0700

    fix: prevent login query from erroring when no profile is found

--- components/Login.js
+++ components/Login.js
@@ -24,7 +24,7 @@ export default function Login({ onLogin }) {
                 .from('profiles')
                 .select('*')
                 .eq('username', username.trim())
-                .single();
+                .maybeSingle();
 
             if (error) throw error;
 
